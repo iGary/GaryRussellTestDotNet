948b8a8c0b61f30a79ed038323e78e3c9faaa84e
Gary Russell <grussell90@gmail.com>
2016-09-16 21:48:24 +0100

    Initial project commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GaryRussell.Startup))]
+namespace GaryRussell
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
